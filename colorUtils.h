240b7b343917a363075ae6cffde7d9c4a971ee9b
HAMAD SAALEH <hammadsalehswabi@gmail.com>
2023-12-20 23:08:55 -0800

    modified:   colorUtils,h

--- colorUtils.h
+++ colorUtils.h
@@ -41,3 +41,9 @@ float toGrayScaleLightness(int red, int green, int blue);
 float toGrayScaleLuminosity(int red,int green,int blue);
 
 // Prototype of toSepiaRed(); function 
+
+float toSepiaRed(int red,int green,int blue);
+
+// Prototype of toSepiaGreen(); function 
+
+float toSepiaGreen(int red,int green,int blue);
